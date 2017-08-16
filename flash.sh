3229fa88b24313f923bc84a9ffb9b78935ff2c83
Martin T. H. Sandsmark <martin.sandsmark@remarkable.no>
2017-08-16 14:07:59 +0200

    add a simple flashing script

---
+++ flash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+scp u-boot.imx zg2:.
+ssh root@10.11.99.1 'echo 0 > /sys/block/mmcblk1boot0/force_ro'
+ssh root@10.11.99.1 'dd if=u-boot.imx of=/dev/mmcblk1boot0 bs=512 seek=2'
+ssh root@10.11.99.1 'echo 1 > /sys/block/mmcblk1boot0/force_ro'
