84b90252eed719638b13ba4208696f4f6ad37ec6
Lars Ivar Miljeteig <lars.ivar.miljeteig@remarkable.no>
2019-05-03 14:07:08 +0200

    zero-sugar: Add sy7636a VCOM in power on
    Check for vcom environment variable to set
    before power up is done.

--- board/reMarkable/zero-sugar/zero-sugar.c
+++ board/reMarkable/zero-sugar/zero-sugar.c
@@ -290,9 +290,15 @@ int board_phy_config(struct phy_device *phydev)
 #define SY7636A_OPERATIONMODE_ONOFF 0x80
 #define SY7636A_OPERATIONMODE_VCOMCTRL 0x40
 
+#define SY7636A_REG_VCOMADJUST_L 0x01
+#define SY7636A_REG_VCOMADJUST_H 0x02
+
+#define SY7636A_VCOMADJUST_LMASK 0xff
+#define SY7636A_VCOMADJUST_HMASK 0x80
+
 static int sy7636a_i2c_reg_write(struct udevice *dev, uint addr, uint mask, uint data)
 {
-	uint8_t valb;
+	u8 valb;
 	int ret;
 
 	if (mask != 0xff) {
@@ -310,12 +316,71 @@ static int sy7636a_i2c_reg_write(struct udevice *dev, uint addr, uint mask, uint
 	return ret;
 }
 
+static int sy7636a_i2c_reg_read(struct udevice *dev, u8 addr, u8 *data)
+{
+	u8 valb;
+	int ret;
+
+	ret = dm_i2c_read(dev, addr, &valb, 1);
+	if (ret)
+		return ret;
+
+	*data = (int)valb;
+	return 0;
+}
+
+static int sy7636a_vcom_get(struct udevice *dev, int *vcom)
+{
+	u8 low, high;
+	int ret;
+
+	ret = sy7636a_i2c_reg_read(dev, SY7636A_REG_VCOMADJUST_L, &low);
+	if (ret)
+		return ret;
+
+	ret = sy7636a_i2c_reg_read(dev, SY7636A_REG_VCOMADJUST_H, &high);
+	if (ret)
+		return ret;
+
+	low &= SY7636A_VCOMADJUST_LMASK;
+	high &= SY7636A_VCOMADJUST_HMASK;
+
+	*vcom = -10 * (low | ((u16)high << 1));
+	*vcom = (*vcom < -5000) ? -5000 : *vcom;
+	return 0;
+}
+
+static int sy7636a_vcom_set(struct udevice *dev, int vcom)
+{
+	u8 high, low;
+	int ret;
+
+	if (vcom < 0)
+		vcom = -vcom;
+
+	vcom /= 10;
+
+	if (vcom > 0x01FF)
+		return -EINVAL;
+
+	low = vcom & SY7636A_VCOMADJUST_LMASK;
+	high = ((u16)vcom >> 1) & SY7636A_VCOMADJUST_HMASK;
+
+	ret = sy7636a_i2c_reg_write(dev, SY7636A_REG_VCOMADJUST_L, SY7636A_VCOMADJUST_LMASK, low);
+	if (ret)
+		return ret;
+
+	return sy7636a_i2c_reg_write(dev, SY7636A_REG_VCOMADJUST_H, SY7636A_VCOMADJUST_HMASK, high);
+}
+
 static int do_epd_power_on(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	struct udevice *bus, *dev;
+	u8 mask, val;
+	ulong vcom;
+	int ivcom;
 
 	int ret;
-	u8 mask, val;
 
 	ret = uclass_get_device_by_seq(UCLASS_I2C, SY7636A_I2C_BUS, &bus);
 	if (ret) {
@@ -330,7 +395,18 @@ static int do_epd_power_on(cmd_tbl_t *cmdtp, int flag, int argc, char * const ar
 		return -1;
 	}
 
-	/* Power on, include VCOM */
+	ret = sy7636a_vcom_get(dev, &ivcom);
+	if (ret)
+		return ret;
+
+	vcom = env_get_ulong("vcom", 10, 1250);
+	printf("vcom was %dmV, setting to -%lumV\n", ivcom, vcom);
+
+	ret = sy7636a_vcom_set(dev, vcom);
+	if (ret)
+		return ret;
+
+	/* Power on, include VCOM in power sequence */
 	mask = (SY7636A_OPERATIONMODE_ONOFF | SY7636A_OPERATIONMODE_VCOMCTRL);
 	val = SY7636A_OPERATIONMODE_ONOFF;
 
