e689349dbe89f36eebbad7aba60808071ecb947c
Steinar Bakkemo <steinar.bakkemo@remarkable.no>
2019-06-12 16:01:04 +0200

    Turn off BT module in WIFI device during initiation (not used yet)

--- board/reMarkable/zero-sugar/wifi_init.c
+++ board/reMarkable/zero-sugar/wifi_init.c
@@ -8,13 +8,17 @@
 
 static iomux_v3_cfg_t const wifi_32K_clk_pad[] = {
     MX7D_PAD_SD1_WP__CCM_CLKO2	| MUX_PAD_CTRL(NO_PAD_CTRL),
+    MX7D_PAD_ECSPI2_SS0__GPIO4_IO23 | MUX_PAD_CTRL (NO_PAD_CTRL)
 };
 
 void zs_do_wifi_poweron_cycle(void)
 {
-    printf("Turning off WIFI power before configuring 32K clock..\n");
+    printf("Turning off WIFI/BT power before configuring 32K clock..\n");
     gpio_request(IMX_GPIO_NR(6, 13), "WIFI_PWR_EN");
     gpio_direction_output(IMX_GPIO_NR(6, 13) , 0);
+
+    gpio_request(IMX_GPIO_NR(4, 23), "BT_REG_EN");
+    gpio_direction_output(IMX_GPIO_NR(4, 23), 0);
 
     printf("Setting IPP_D0_CLKO2 to get input from OSC_32K_CLK..\n");
     clock_set_src(IPP_DO_CLKO2, OSC_32K_CLK);
